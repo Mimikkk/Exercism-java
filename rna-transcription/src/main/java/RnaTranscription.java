47f6ac52ba0e7922940e98915e848a187c3214e3
Mimikkk <Inwazzin@gmail.com>
2020-10-01 18:18:22 +0200

    Easy-problems

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,19 @@
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class RnaTranscription {
+    private static final HashMap<Character, Character> translation = new HashMap<>()
+    {{
+        put('G','C');
+        put('C','G');
+        put('T','A');
+        put('A','U');
+    }};
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // "ACGT" -> "UGCA"
+        return dnaStrand.chars().mapToObj(c->translation.get(((char) c)).toString()).collect(Collectors.joining(""));
     }
 
 }
