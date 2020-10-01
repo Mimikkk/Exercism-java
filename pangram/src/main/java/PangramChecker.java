7f15f38da934e3c8525a2146af01388b2d7a8685
Mimikkk <Inwazzin@gmail.com>
2020-10-02 00:25:29 +0200

    Thankfully-there-are-collectors

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,17 @@
-public class PangramChecker {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class PangramChecker {
+    private static final HashSet<String> alphabet_set =
+            "abcdefghijklmnopqrstuvwxyz".chars().mapToObj(Character::toString)
+                    .collect(Collectors.toCollection(HashSet::new));
+
+    public boolean isPangram(String str) {
+        HashSet<String> str_set =
+                str.chars().mapToObj(c -> Character.toString(c).toLowerCase())
+                        .collect(Collectors.toCollection(HashSet::new));
+        return str_set.containsAll(alphabet_set);
     }
 
 }
