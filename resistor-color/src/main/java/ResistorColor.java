5659cb31b83ec44d7fd99d96a91fa17ad27390dd
Mimikkk <Inwazzin@gmail.com>
2020-10-01 15:10:04 +0200

    Easy-problems

--- resistor-color/src/main/java/ResistorColor.java
+++ resistor-color/src/main/java/ResistorColor.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
+import java.util.List;
 
 class ResistorColor {
-    String[] colors = new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    private final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
 
     int colorCode(String targetColor) {
-        int index = Arrays.binarySearch(this.colors, targetColor);
-        return (index < 0) ? -1 : index + 1;
+        return Arrays.asList(colors).indexOf(targetColor);
     }
 
     String[] colors() {
-        return colors.clone();
+        return colors;
     }
 }
