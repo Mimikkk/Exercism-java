21e92177fb547e63820e2e72eca9758080e52bd3
Mimikkk <Inwazzin@gmail.com>
2020-10-02 00:57:50 +0200

    Thankfully-there-are-collectors

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,9 +1,26 @@
 public class Hamming {
-    public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private int HAMMING_DISTANCE = 0;
+
+
+    public Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
+        if (rightStrand.isEmpty() && !leftStrand.isEmpty()) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        if (leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                HAMMING_DISTANCE ++;
+            }
+        }
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return HAMMING_DISTANCE;
     }
 }
