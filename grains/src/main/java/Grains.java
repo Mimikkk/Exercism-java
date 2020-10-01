e33c2d60bc8261fc0e7ae1414ed908fe0ab85d74
Mimikkk <Inwazzin@gmail.com>
2020-10-02 01:31:09 +0200

    Thankfully-there-are-collectors

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -2,12 +2,14 @@ import java.math.BigInteger;
 
 class Grains {
 
-    BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    BigInteger grainsOnSquare(final int square) throws IllegalArgumentException {
+        if (square < 1 || square > 64) throw new IllegalArgumentException("square must be between 1 and 64");
+
+        return new BigInteger(String.valueOf(1)).shiftLeft(square-1);
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new BigInteger("18446744073709551615");
     }
 
 }
